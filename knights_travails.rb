c418801e644f6d189d935aa53439f0c91f78f429
Cody Buffaloe <codylbuffaloe@gmail.com>
2017-05-24 17:32:29 -0700

    Added directions for project in comments, attempted to make an array of arrays for the board, not quite working

--- knights_travails.rb
+++ knights_travails.rb
@@ -6,4 +6,15 @@ class Knight
 end
 
 class Board
+  def initialize
+    @board = Array.new(8){Array.new(8)}
+    @board.each do |e|
+      puts "#{e}"
+    end
+  end
 end
+
+b = Board.new
+#Build a board and a knight
+#treat all possible Knight moves as children in a tree NO MOVES OFFBOARD!
+#Which search algorithm is best for this? What is good at going through a potentially infinite series quickly?
